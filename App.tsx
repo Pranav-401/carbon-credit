2c4b83f09823a09c35a744539dcda3891d614c17
Pranav-401 <pranavmaurya401@gmail.com>
2025-08-30 22:24:13 +0530

    android

--- App.tsx
+++ App.tsx
@@ -1,13 +1,40 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
-import './global.css';
+import React from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
 
 export default function App() {
+  const offset = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offset.value * 100 }],
+    };
+  });
+
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+    <View style={styles.container}>
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <Button
+        title="Move Box"
+        onPress={() => {
+          offset.value = withSpring(Math.random() * 3);
+        }}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 80,
+    height: 80,
+    backgroundColor: 'skyblue',
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+});
